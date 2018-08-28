0bffa3c3a011c4db7c5e635e1e1aac7da606df3f
xiong34664 <xjh34664@163.com>
2018-08-28 17:35:35 +0800

    pushu8/28

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -4,19 +4,39 @@ import {
   Route,
   Link,
   NavLink,
-  Redirect
+  Redirect,Switch
 } from 'react-router-dom'
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+
 import Df from './pages/Df'
 import Lb from './pages/Lb'
 import Ss from './pages/Ss'
 import Content from './pages/Content'
 import './App.css'
 
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
+
 class App extends Component {
+  
   render() {
+    console.log(this)
     return ( 
       <Router>
-        <div className="App">
+        <Route render={({location})=>(
+          <div className="App">
           <h1>Simple SAP</h1>
           <ul>
             <li><NavLink activeClassName="selected" to="/lb">李白</NavLink></li>
@@ -24,13 +44,20 @@ class App extends Component {
             <li><NavLink activeClassName="selected" to="/ss">苏轼</NavLink></li>
           </ul>
           <div className="content">
-          <Route exact path="/" render={() => <Redirect to="/lb" />}></Route>
-          <Route path="/lb"  component={Lb} />
-          <Route path="/df" component={Df}/>
-          <Route path="/ss" component={Ss}/>
-          <Route path="/:type" component={Content}/>
+          <TransitionGroup>
+            <Route exact path="/" render={() => <Redirect to="/lb" />} />
+             <CSSTransition key={location.key} classNames="example" timeout={{ enter: 500, exit: 300 }} >
+              <Switch >
+                <Route exact  path="/lb"  component={Lb} />
+                <Route exact  path="/df" component={Df}/>
+                <Route exact  path="/ss" component={Ss}/>
+                <Route component={Lb}/>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
           </div>
         </div>
+        )} />
       </Router>
     )
   }
--- router-app/src/pages/Lb/index.jsx
+++ router-app/src/pages/Lb/index.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-export default function Lb () {
+import React,{Component} from 'react'
+import './style.css'
+export default class Lb extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: true
+    }
+  }
+  onChange = (e) => {
+    const length = e.target.value.length;
+    if (length >= 4) {
+      this.setState(() => ({ disabled: false }))
+    } else if (!this.state.disabled) {
+      this.setState(() => ({ disabled: true }))
+    }
+  }
+  
+  render() {
+    const label = this.state.disabled ? 'Disabled' : 'Submit';
   return <div>
     <h2>《望庐山瀑布》</h2>
     <p>日照香炉生紫烟，</p>
     <p>遥看瀑布挂前川。</p>
     <p>飞流直下三千尺，</p>
     <p>疑是银河落九天。</p>
-
+    <button className={!this.state.disabled && 'buttonEnabled'} disabled={this.state.disabled}>{label}</button>
+    <input onChange={this.onChange}/>
   </div>
+  }
 }
