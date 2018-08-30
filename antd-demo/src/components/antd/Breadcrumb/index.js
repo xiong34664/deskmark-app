6a774e23bba8722f5f0b46f4d0aa944871d76b4a
xiong34664 <xjh34664@163.com>
2018-08-30 15:44:31 +0800

    8/30

--- antd-demo/src/components/antd/Breadcrumb/index.js
+++ antd-demo/src/components/antd/Breadcrumb/index.js
@@ -25,7 +25,6 @@ const Home = withRouter((props) => {
   const pathSnippets = location.pathname.split('/').filter(i => i);
   const extraBreadcrumbItems = pathSnippets.map((_, index) => {
     const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
-    console.log(url)
     return (
       <Breadcrumb.Item key={url}>
         <Link to={url}>
